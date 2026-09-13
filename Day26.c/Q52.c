5c64c28dfdbbf751c1a283c3cbdcdc072b6654d7
Arush Panthri <arushpanthri21@gmail.com>
2026-09-13 09:39:17 +0530

    Add files via upload

---
+++ Day26.c/Q52.c
@@ -0,0 +1,44 @@
+//Q52: Write a program to print the following pattern:
+/*
+
+*
+
+***
+
+*****
+
+***
+
+*
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {
+
+        if (i <= 3) {
+            for (j = 1; j <= 2 * i - 1; j++) {
+                printf("*");
+            }
+        } else {
+            for (j = 1; j <= 11 - 2 * i; j++) {
+                printf("*");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
